28d0757afe5bfde0c778f5318ba9a06040709381
trad <trad0000@google.com>
2020-06-25 01:47:38 +0300

    ''

--- consts.h
+++ consts.h
@@ -3,3 +3,12 @@
 #define ENC_B D7
 #define BUZZER D0
 #define DSPL_ADDR 0x27
+
+#define ONE_WIRE_BUS D3
+#define BMP_SDA_BUS D2
+#define BMP_SCL_BUS D1
+#define BMP_PRESSURE0 1013.25
+
+#define DS18_RESOLUTION 9
+
+const float emptySignal = -127;
--- duremar4.ino
+++ duremar4.ino
@@ -1,81 +1,85 @@
-#include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson
-#include <LiquidCrystal_PCF8574.h>
-#include <Button.h>
+#include <Button2.h>
 #include <ESPRotary.h>
 
+#include "consts.h"
+#include "pitches.h"
+#include "Config.h"
+#include "AMain.h"
 
-#include "ConfigNavigate.h"
-#define ENC_BTN D5
-#define ENC_A D6
-#define ENC_B D7
+enum MODES { MAIN, CONFIG };
+MODES mode = MAIN;
+//MODES mode = CONFIG;
 
-#define LCD_COLS 20
-#define LCD_ROWS 4
-LiquidCrystal_PCF8574 lcd(0x27);
-
-Button button(ENC_BTN);
+Button2 button(ENC_BTN);
 ESPRotary rotary(ENC_A, ENC_B, 4);
-ConfigNavigate cn(ENC_A, ENC_B, ENC_BTN, lcd, LCD_ROWS);
 
-void buttonPressed() {
-  Serial.println("main buttonPressed");
-  Serial.print("cn.started() ");
-  Serial.println(cn.started());
-  cn.begin();
+void onEncLeft(ESPRotary& rotary) {
+  if (mode == CONFIG)
+    config::onEncLeft(rotary);
+  else {
+    main::page = 1 - main::page;
+    main::draw(true);
+    main::doSound(100, 5);
+  }
 }
 
-void rotRight(ESPRotary& rotary) {
-  Serial.println("main rotRight");
+void onEncRight(ESPRotary& rotary) {
+  if (mode == CONFIG)
+    config::onEncRight(rotary);
+  else {
+    main::page = 1 - main::page;
+    main::draw(true);
+    main::doSound(100, 5);
+  }
 }
 
-void rotLeft(ESPRotary& rotary) {
-  Serial.println("main rotLeft");
-}
-
-void draw() {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("DUREMAR 4");
+void buttonPressed(Button2& btn) {
+  if (mode == CONFIG)
+    config::buttonPressed(btn);
+  else {
+    mode = CONFIG;
+    config::begin();
+  }
 }
 
 void onConfigExit() {
-  draw();
+  mode = MAIN;
+  main::init();
+  main::draw(true);
 }
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
   
-  lcd.begin(LCD_COLS, LCD_ROWS);
-  lcd.setBacklight(true);
-  lcd.home();
-  
   pinMode(ENC_A, INPUT_PULLUP);
   pinMode(ENC_B, INPUT_PULLUP);
   pinMode(ENC_BTN, INPUT_PULLUP);
-  button.begin();
-  rotary.setRightRotationHandler(rotRight);
-  rotary.setLeftRotationHandler(rotLeft);
-  cn.setConfigExit(onConfigExit);
+  pinMode(BUZZER, OUTPUT);
+
+  button.setPressedHandler(buttonPressed);
+  rotary.setRightRotationHandler(onEncRight);
+  rotary.setLeftRotationHandler(onEncLeft);
   
-//  cn.begin();
-  draw();
+  config::setup(BUZZER, onConfigExit);
+
+  main::setup();
+  main::init();
+  
+  if (mode == CONFIG)
+    config::begin();
+  else
+    main::draw(true);
+
+//  if (config::conf["soundOn"])
+//    startMelodyPlay();
 }
 
-unsigned long lastUpdate = 0;
-unsigned long updateInterval = 1000;
-
 void loop() {
-  if (cn.started()) {
-    cn.loop();
-  } else {
-    rotary.loop();
-    if (button.toggled())
-      if (button.read() == Button::PRESSED)
-        buttonPressed();
-  }
-
-  if ((millis() - lastUpdate >= updateInterval) || lastUpdate == 0) {
-    lastUpdate = millis();
+  rotary.loop();
+  button.loop();
+  if (mode == MAIN){
+    main::loop();
+    main::draw();
   }
 }
